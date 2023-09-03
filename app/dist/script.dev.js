e1503d8c5980094b36ce7a81c1d05a1c142e0a22
jacobharraldson <jharraldson@gmail.com>
2023-09-03 10:42:33 +0200

    thank you screen added/js logic for form validation and ty screen added

--- app/dist/script.dev.js
+++ app/dist/script.dev.js
@@ -3,6 +3,10 @@
 var submitBtn = document.querySelector('.newsletter__submit');
 var inputField = document.querySelector('.newsletter__input');
 var formErr = document.querySelector('.newsletter__formErr');
+var sectionMain = document.querySelector('.newsletter__wrapper');
+var sectionTy = document.querySelector('.newsletter__tyScreen');
+var dismissBtn = document.querySelector('.-dismissBtn');
+sectionTy.classList.add('-hidden');
 submitBtn.addEventListener('click', function (e) {
   e.preventDefault();
   var inputContent = inputField.value;
@@ -10,8 +14,14 @@ submitBtn.addEventListener('click', function (e) {
   if (inputContent.includes('@')) {
     formErr.classList.add('-hidden');
     inputField.classList.remove('-errState');
+    sectionMain.classList.add('-hidden');
+    sectionTy.classList.remove('-hidden');
   } else {
     inputField.classList.add('-errState');
     formErr.classList.remove('-hidden');
   }
+});
+dismissBtn.addEventListener('click', function () {
+  sectionMain.classList.remove('-hidden');
+  sectionTy.classList.add('-hidden');
 });
--- app/script.js
+++ app/script.js
@@ -1,7 +1,12 @@
 const submitBtn = document.querySelector('.newsletter__submit');
 const inputField = document.querySelector('.newsletter__input');
 const formErr = document.querySelector('.newsletter__formErr');
+const sectionMain = document.querySelector('.newsletter__wrapper');
+const sectionTy = document.querySelector('.newsletter__tyScreen');
+const dismissBtn = document.querySelector('.-dismissBtn');
 
+
+sectionTy.classList.add('-hidden')
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -10,8 +15,15 @@ submitBtn.addEventListener('click', (e) => {
     if (inputContent.includes('@')) {
         formErr.classList.add('-hidden')
         inputField.classList.remove('-errState')
+        sectionMain.classList.add('-hidden')
+        sectionTy.classList.remove('-hidden')
     } else {
         inputField.classList.add('-errState')
         formErr.classList.remove('-hidden')
     }
+})
+
+dismissBtn.addEventListener('click', () => {
+    sectionMain.classList.remove('-hidden')
+    sectionTy.classList.add('-hidden')
 })
